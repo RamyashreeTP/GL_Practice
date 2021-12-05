37448b72da4d994ea753fcaf2f5a53255ff7b770
RamyashreeTP <42921244+RamyashreeTP@users.noreply.github.com>
2021-12-05 21:47:08 +0530

    Sprint II assessment

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace ProjectManagement.Models
 {
     public class LoginModel
     {
+        [Key]
         public string userName { get; set; }
         public string userPassword { get; set; }
     }
 }
+
