574b460eb8724f07e64b92ff961f51dc3a425bec
RamyashreeTP <42921244+RamyashreeTP@users.noreply.github.com>
2021-12-05 22:23:01 +0530

    Sprint II assessment

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,10 +28,11 @@ namespace ProjectManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IUserAuthentication, UserAuthentication>();
-            services.AddSingleton<IUserProvider, UserProvider>();
-            services.AddSingleton<IProjectProvider, ProjectProvider>();
-            services.AddSingleton<ITaskProvider, TaskProvider>();
+            services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("ProjectManagement"));
+            services.AddScoped<IUserAuthentication, UserAuthentication>();
+            services.AddScoped<IUserProvider, UserProvider>();
+            services.AddScoped<IProjectProvider, ProjectProvider>();
+            services.AddScoped<ITaskProvider, TaskProvider>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
